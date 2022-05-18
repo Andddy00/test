fdb8027324d363b41cdf2e7d024e7e117d3c4565
Andddy00 <229830916@qq.com>
2022-05-18 18:15:48 +0800

    master

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+	public static void main(String[] args){
+		System.out.println(0x10);
+		System.out.println(5.5);
+		System.out.println('a');
+		System.out.println(true);
+		System.out.println("halloworld");
+	}
+}
