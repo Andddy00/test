7360a8c700fd7f3d3cb077baf0f0543f7e049119
Andddy00 <229830916@qq.com>
2022-05-19 09:27:15 +0800

    HelloWorld

--- Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo{
-	public static void main(String[] args){
-		System.out.println(0x10);
-		System.out.println(5.5);
-		System.out.println('a');
-		System.out.println(true);
-		System.out.println("halloworld");
-	}
-}
--- ScannerDemo1.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class ScannerDemo1{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("putin");
-		int i = sc.nextInt()+1;
-		System.out.println(i);
-	}
-}
